7c392461cbb83e28cea8488952c9641b560be6cb
Matthew Harrigan <betterthanbossfiredog5@gmail.com>
2020-12-29 00:09:02 -0800

    I always forget to update the version

--- src/main/java/serenetweaks/core/SereneTweaks.java
+++ src/main/java/serenetweaks/core/SereneTweaks.java
@@ -16,7 +16,7 @@ public class SereneTweaks {
 
 	public static final String MODID = "serenetweaks";
 	public static final String NAME = "Serene Tweaks";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "1.0.0";
 	public static final String MC_VERSION = "[1.12.2]";
 
 	public static final Logger LOGGER = LogManager.getLogger(SereneTweaks.MODID);
